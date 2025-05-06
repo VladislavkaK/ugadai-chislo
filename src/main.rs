6ae0392fe61eaab0be9bc5c9e38f3930657f88ce
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2025-05-06 23:47:47 +0300

    added solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Игра: Угадай число!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+        println!("Введите число:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Ошибка ввода!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Пожайлуста, введите число!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком маленькое!"),
+            Ordering::Greater => println!("Слишком большое!"),
+            Ordering::Equal => {
+                println!("Угадал!");
+                break;
+            }
+        }
+    }
 }
